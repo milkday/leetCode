6101bce27e6e62e8fea6dbd40e06d1ac9c8069ce
zuozhongfei <>
2020-04-07 16:11:32 +0800

    有效的括号第二种算法添加注释

--- src/validParentheses/Solution.java
+++ src/validParentheses/Solution.java
@@ -16,23 +16,25 @@ import java.util.Stack;
 * */
 
 public class Solution {
-    private HashMap<Character,Character> mappings;
-    public Solution(){
+    private HashMap<Character, Character> mappings;
+
+    public Solution() {
         this.mappings = new HashMap<Character, Character>();
-        this.mappings.put(')','(');
-        this.mappings.put('}','{');
-        this.mappings.put(']','[');
+        this.mappings.put(')', '(');
+        this.mappings.put('}', '{');
+        this.mappings.put(']', '[');
     }
-    public boolean isValid(String s){
+
+    public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (this.mappings.containsKey(c)){
+            if (this.mappings.containsKey(c)) {
                 char topElement = stack.empty() ? '#' : stack.pop();
-                if (topElement != this.mappings.get(c)){
+                if (topElement != this.mappings.get(c)) {
                     return false;
                 }
-            }else{
+            } else {
                 stack.push(c);
             }
         }
@@ -40,7 +42,7 @@ public class Solution {
     }
 
     @Test
-    public void testSolution(){
+    public void testSolution() {
         String string = "{}{}{}()()[]";
 
         boolean valid = isValid(string);
--- src/validParentheses/Solution1.java
+++ src/validParentheses/Solution1.java
@@ -8,26 +8,27 @@ import java.util.Stack;
 
 public class Solution1 {
 
-    public boolean isValid(String s){
-        if (s.length()==0) return true;
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == '(') {
-                stack.push(')');
-            }else if (c == '['){
-                stack.push(']');
-            }else if (c == '{'){
-                stack.push('}');
-            }else if (stack.isEmpty() || c != stack.pop()){
-                return false;
+    public boolean isValid(String s) {
+        if (s.length() == 0) return true;                 //如果字符串为空,返回true
+        Stack<Character> stack = new Stack<>();         //创建栈对象:stack
+        for (int i = 0; i < s.length(); i++) {          //遍历传进来的字符串的每一个字符
+            char c = s.charAt(i);                       //取出单个字符
+            if (c == '(') {                             //如果匹配到'(' 开始的小括号
+                stack.push(')');                       //向栈中入栈一个')'
+            } else if (c == '[') {                      //如果匹配到'['
+                stack.push(']');                       //入栈']'
+            } else if (c == '{') {                      //如果匹配到'{'
+                stack.push('}');                       //入栈'}'
+            } else if (stack.isEmpty()                  //如果匹配到的字符不是'(','[','{' 这几个开始括号
+                    || c != stack.pop()) {                  //如果栈为空,或者当前单个字符,不等于出栈的字符
+                return false;                               //说明不是一个所有括号有效闭合的字符串
             }
         }
-        return stack.isEmpty();
+        return stack.isEmpty();                         //最后遍历完字符串,如果栈为空,就返回true,不为空,表示还要元素没有出栈,返回false
     }
 
     @Test
-    public void testSolution1(){
+    public void testSolution1() {
         String string = "{}{}{}()([]";
 
         boolean valid = isValid(string);
@@ -35,7 +36,7 @@ public class Solution1 {
     }
 
     @Test
-    public void testSolution2(){
+    public void testSolution2() {
         String string = "{}{}{}()[]";
 
         boolean valid = isValid(string);
